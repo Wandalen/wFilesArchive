665893c32a858c13d4851e17a21b3f494b2be95a
Wandalen <wandalen.me@gmail.com>
2018-11-01 16:28:59 +0200

    auto

--- staging/dwtools/amid/files/l9/FilesArchive.s
+++ staging/dwtools/amid/files/l9/FilesArchive.s
@@ -17,15 +17,7 @@ let _ = _global_.wTools;
 let Parent = null;
 let Self = function wFilesArchive( o )
 {
-  if( !( this instanceof Self ) )
-  if( o instanceof Self )
-  {
-    _.assert( arguments.length === 1, 'Expects single argument' );
-    return o;
-  }
-  else
-  return new( _.routineJoin( Self, Self, arguments ) );
-  return Self.prototype.init.apply( this,arguments );
+  return _.instanceConstructor( Self, this, arguments );
 }
 
 Self.shortName = 'FilesArchive';
