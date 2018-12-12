5ca25acfab2233f7d2fa2d104d8e26313a1be6c0
Wandalen <wandalen.me@gmail.com>
2018-12-12 13:54:49 +0200

    auto commit

---
+++ out.debug/dwtools/amid/files/l1/ArchiveRecordFactory.s
@@ -0,0 +1,155 @@
+( function _AtchiveRecordFactory_s_() {
+
+'use strict';
+
+//
+
+let _global = _global_;
+let _ = _global_.wTools;
+let Parent = null;
+let Self = function wArchiveRecordFactory( o )
+{
+  return _.instanceConstructor( Self, this, arguments );
+}
+
+Self.shortName = 'ArchiveRecordFactory';
+
+// --
+// inter
+// --
+
+function finit()
+{
+  let self = this;
+  _.Copyable.prototype.finit.apply( self, arguments );
+}
+
+//
+
+function init( o )
+{
+  let self = this;
+
+  _.instanceInit( self );
+  if( o )
+  self.copy( o );
+  Object.preventExtensions( self );
+
+  self.records.filePath = self.records.filePath || Object.create( null );
+}
+
+//
+
+function record( o )
+{
+  let self = this;
+
+  if( _.strIs( o ) )
+  o = { absolute : o }
+
+  _.assert( arguments.length === 1 );
+  _.assert( _.strIs( o.absolute ) );
+  _.assert( o.factory === self || o.factory === undefined );
+
+  o.factory = self;
+
+  let record = _.ArchiveRecord( o );
+
+  return record;
+}
+
+//
+
+function recordsSelect( filePath )
+{
+  let self = this;
+
+  let result = _.mapVals( _.mapSelect( self.records.filePath, filePath ) ).filter( ( el ) => el !== undefined );
+
+  return result;
+}
+
+// --
+//
+// --
+
+let Composes =
+{
+}
+
+let Aggregates =
+{
+}
+
+let Associates =
+{
+  records : _.define.ownInstanceOf( _.FileRecordFilter ),
+  originalFileProvider : null,
+  imageFileProvider : null,
+}
+
+let Restricts =
+{
+}
+
+let Medials =
+{
+}
+
+let Statics =
+{
+}
+
+let Forbids =
+{
+}
+
+// --
+// declare
+// --
+
+let Extend =
+{
+
+  finit,
+  init,
+
+  record,
+  recordsSelect,
+
+  //
+
+  Composes,
+  Aggregates,
+  Associates,
+  Restricts,
+  Medials,
+  Statics,
+  Forbids,
+
+}
+
+//
+
+_.classDeclare
+({
+  cls : Self,
+  parent : Parent,
+  extend : Extend,
+});
+
+_.Copyable.mixin( Self );
+_[ Self.shortName ] = Self;
+
+// --
+// export
+// --
+
+// if( typeof module !== 'undefined' )
+// if( _global_.WTOOLS_PRIVATE )
+// { /* delete require.cache[ module.id ]; */ }
+
+if( typeof module !== 'undefined' && module !== null )
+module[ 'exports' ] = Self;
+
+})();
