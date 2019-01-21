1530f734d047c4a128fb0f1073ae39dea8f85258
vmigdal <volodymyr.migdal@gmail.com>
2019-01-21 11:30:11 +0200

    upgrade

--- out.debug/dwtools/amid/files/l9/GraphOld.s
+++ out.debug/dwtools/amid/files/l9/GraphOld.s
@@ -3,7 +3,8 @@
 'use strict';
 
 /**
-  @module Tools/mid/FilesArchive - Experimental. Several classes to reflect changes of files on dependent files and keep links of hard linked files. FilesArchive provides means to define interdependence between files and to forward changes from dependencies to dependents. Use FilesArchive to avoid unnecessary CPU workload.
+ * Experimental. Several classes to reflect changes of files on dependent files and keep links of hard linked files. FilesArchive provides means to define interdependence between files and to forward changes from dependencies to dependents. Use FilesArchive to avoid unnecessary CPU workload.
+  @module Tools/mid/FilesArchive
 */
 
 /**
